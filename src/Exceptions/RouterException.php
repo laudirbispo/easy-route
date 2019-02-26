45ceadb40d20c46d69bd21ecbb01d87e1a7be726
laudirbispo <laudirbispo@pitlook.com>
2019-02-26 12:35:39 -0300

    Fixed namespace error

--- src/Exceptions/RouterException.php
+++ src/Exceptions/RouterException.php
@@ -1,5 +1,5 @@
 <?php declare(strict_types=1);
-namespace laudirbispo\EasyRoutere\Exceptions;
+namespace laudirbispo\EasyRouter\Exceptions;
 /**
  * Copyright (c) Laudir Bispo  (laudirbispo@outlook.com)
  *
